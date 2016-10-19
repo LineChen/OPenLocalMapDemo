063e48745b1a4f89972e91161982ec75c5ec1b06
Beiing <15764230067@163.com>
2016-10-19 17:57:50 +0800

    first commit

---
+++ app/src/main/java/com/beiing/openlocalmapdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.beiing.openlocalmapdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openBaiduMap(View view) {
+
+    }
+
+    public void openWebBaiduMap(View view) {
+    }
+
+    public void openGaodeMap(View view) {
+
+    }
+}
